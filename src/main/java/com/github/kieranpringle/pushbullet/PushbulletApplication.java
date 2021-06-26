95f36d04bd8b1e705208a38c8daa2ab38a7879a9
Kieran Pringle <kpringle@adobe.com>
2021-06-26 08:57:53 +0100

    Init repo
    Commits the output of IntelliJ's mvn + SpringBoot project generator

---
+++ src/main/java/com/github/kieranpringle/pushbullet/PushbulletApplication.java
@@ -0,0 +1,13 @@
+package com.github.kieranpringle.pushbullet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PushbulletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PushbulletApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/kieranpringle/pushbullet/PushbulletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kieranpringle.pushbullet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PushbulletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
